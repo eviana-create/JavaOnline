72f2fb0fb2868a46ed8c2fe59443ffb8e1a7d8ac
Emerson Viana <missonzito@outlook.com>
2023-03-24 11:16:11 -0300

    Continuação Tipos primitivos, exercícios e tipos de operadores

---
+++ src/imprimindoVariaveisContinuacaoDaContinuacao.java
@@ -0,0 +1,12 @@
+public class imprimindoVariaveisContinuacaoDaContinuacao {
+    public static void main(String[] args) {
+        String nome = "Emerson Viana";
+        float  salario = 3000;
+        String sexo = "M";
+        int idade = 39;
+        String estadoCivil = "Casado(Namorando)";
+
+        System.out.println("O Trabalhador "+nome+" do sexo "+sexo+" e "+idade+" anos de idade, que encontra-se "+estadoCivil+" recebendo "+salario+" reais de pagamento, está trabalhando neste estabelecimento, só que não néh rs.");
+
+    }
+}
