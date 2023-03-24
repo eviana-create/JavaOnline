09ffe7e65dfd3cf9101d6e6d5beb26bd5cd4ef22
Emerson Viana <missonzito@outlook.com>
2023-03-24 16:04:27 -0300

    Operadores de Adição, Subtração, Mutiplicação e Divisão.

--- src/operadores.java
+++ src/operadores.java
@@ -1,8 +1,29 @@
 public class operadores {
     public static void main(String[] args) {
+
+        //Operador de Adição
         int n1 = 10;
         int n2 = 20;
         int soma = n1 + n2;
         System.out.println("A soma entre 10 + 20 é = " +soma);
+
+        //Operador de Subtração
+        int n3 = 10;
+        int n4 = 20;
+        int subtracao = n3 + n4;
+        System.out.println("A subtração entre 10 - 20 é = " +(n3 - n4));
+
+        //Operador de Mutiplicação
+        int n5 = 10;
+        int n6 = 20;
+        int mutiplicacao = n5 + n6;
+        System.out.println("A Mutiplicação entre 10 x 20 é = " +(n3 * n4));
+
+        //Operador de Divisão
+        float n7 = 10;
+        float n8 = 20;
+        float Divisão = n7 + n8;
+        System.out.println("A Divisão entre 10 : 20 é = " +(n7 / n8));
+
     }
 }
