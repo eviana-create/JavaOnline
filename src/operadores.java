bd3d4d2d9d5b33d6bd05741e331d8920e613ce8e
Emerson Viana <missonzito@outlook.com>
2023-03-24 11:21:05 -0300

    operador de adição.

---
+++ src/operadores.java
@@ -0,0 +1,8 @@
+public class operadores {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 20;
+        int soma = n1 + n2;
+        System.out.println("A soma entre 10 + 20 é = "+soma);
+    }
+}
