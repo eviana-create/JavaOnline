00aed090df076a53ce595c26a9e7d1db23e7aa15
Emerson Viana <missonzito@outlook.com>
2023-03-24 10:04:14 -0300

    Tipos primitivos, convenções de código e comentários

---
+++ src/ImprimindoVariaveis.java
@@ -0,0 +1,7 @@
+public class ImprimindoVariaveis {
+      public static void main(String[] args) {
+          int idade = 39;
+        System.out.println("Eu tenho " + idade + " Anos de Vida !");
+
+      }
+}
