3185a76bff4aaf7410a6825555b0d9cf02df4ec7
Emerson Viana <missonzito@outlook.com>
2023-03-24 10:44:30 -0300

    Continuação tipos primitivos, exercício e operadores

--- src/ImprimindoVariaveis.java
+++ src/ImprimindoVariaveis.java
@@ -1,7 +1,25 @@
 public class ImprimindoVariaveis {
       public static void main(String[] args) {
           int idade = 39;
-        System.out.println("Eu tenho " + idade + " Anos de Vida !");
+          double salarioDouble = 3000;
+          float salarioFloat = 3000;
+          byte idadeByte = 39;
+          short idadeShort = (short) 32767;
+          boolean verdadeiro = true;
+          boolean falso = false;
+          long numeroGrande = 1000l;
+          char caractere = '\u0041'; // Tipo 'char' obsoleto, utiliza-se string ao invés de char, letra A representada pelo numero exadecimal 0041..
+          String nome = "Emerson Viana";
+          System.out.println("Eu tenho " + idade + " Anos de Vida !");
+          System.out.println(salarioDouble);
+          System.out.println(salarioFloat);
+          System.out.println(idadeByte);
+          System.out.println(idadeShort);
+          System.out.println(verdadeiro);
+          System.out.println(falso);
+          System.out.println(numeroGrande);
+          System.out.println(caractere);
+          System.out.println(nome);
 
       }
 }
