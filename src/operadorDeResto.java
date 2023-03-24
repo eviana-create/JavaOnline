c1f94eb34fea33d35105554651d1fe9b88f61e5f
Emerson Viana <missonzito@outlook.com>
2023-03-24 16:15:57 -0300

    Operador de Resto da Divisão.

---
+++ src/operadorDeResto.java
@@ -0,0 +1,13 @@
+public class operadorDeResto {
+    public static void main(String[] args) {
+        int numeroInteiro = 10;
+        long numeroLong = 200000l;
+
+        double numeroDouble = 10d;
+        float numeroFloat = 20f;
+        numeroFloat = (float) numeroDouble;
+        System.out.println("O Resto da Divisão entre 20 : 2 é igual a " +(20%2));
+        System.out.println(numeroDouble);
+
+    }
+}
