574e8d04d2daa1cc33016738b32d5ddf50b4c494
Emerson Viana <missonzito@outlook.com>
2023-03-24 16:29:06 -0300

    Operadores de Comparação.

---
+++ src/operadoresDeComparacao.java
@@ -0,0 +1,16 @@
+public class operadoresDeComparacao {
+    public static void main(String[] args) {
+        boolean dezMaiorQueVinte = 10 > 20;
+        boolean dezMaiorOuIgualVinte = 10 >= 20;
+        boolean dezMenorQueVinte = 10 < 20;
+        boolean dezMenorOuIgualQueVinte = 10 <= 20;
+        boolean dezIgualAVinte = 10 == 20;
+        boolean dezDiferenteDeVinte = 10 != 20;
+        System.out.println(dezMaiorQueVinte);
+        System.out.println(dezMaiorOuIgualVinte);
+        System.out.println(dezMenorQueVinte);
+        System.out.println(dezMenorOuIgualQueVinte);
+        System.out.println(dezIgualAVinte);
+        System.out.println(dezDiferenteDeVinte);
+    }
+}
