f72c44903844b4c3fd60d40f90b6ae5cd6ec0ee6
Emerson Viana <missonzito@outlook.com>
2023-03-24 10:43:28 -0300

    Continuação tipos primitivos, exercício e operadores

---
+++ src/imprimindoVariaveisContinuacao.java
@@ -0,0 +1,8 @@
+public class imprimindoVariaveisContinuacao {
+    public static void main(String[] args) {
+        String nome = "Emerson Viana";
+        String end = "Estrada dos Alvarengas";
+        int tel = 974200850;
+        System.out.println("O " + nome + ", Domiciliado no Endereço " + end + " e telefone " + tel + ", Não possui nenhuma pendencia.");
+    }
+}
